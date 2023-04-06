5c3d3ab15fcdcd70649b22d535dca61c867df9bc
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-06 19:43:29 +0200

    Display cities and handle click

--- WeatherApplication/AppService/wwwroot/js/drawDiagrams.js
+++ WeatherApplication/AppService/wwwroot/js/drawDiagrams.js
@@ -17,37 +17,16 @@
         type: 'scattergeo',
         mode: 'markers+text',
         text: cityNames,
-        //text: [
-        //    'Berlin 15C', 'Toronto', 'Vancouver', 'Calgary', 'Edmonton',
-        //    'Ottawa', 'Halifax', 'Victoria', 'Winnepeg', 'Regina'
-        //],
         lat: latitudes,
-        //lat: [
-        //    52.5170365, 43.4, 49.13, 51.1, 53.34, 45.24,
-        //    44.64, 48.25, 49.89, 50.45
-        //],
         lon: longitudes,
-        //lon: [
-        //    13.3888599, -79.24, -123.06, -114.1, -113.28,
-        //    -75.43, -63.57, -123.21, -97.13, -104.6
-        //],
         marker: {
             size: 12,
-            color: cities.map(() => '#bebada'),
-            //color: [
-            //    '#bebada', '#fdb462', '#fb8072', '#d9d9d9', '#bc80bd',
-            //    '#b3de69', '#8dd3c7', '#80b1d3', '#fccde5', '#ffffb3'
-            //],
+            color: '#bebada',
             line: {
                 width: 1
             }
         },
-        name: 'Europeian cities',
-        textposition: cities.map(() => 'top'),
-        //textposition: [
-        //    'top', 'top left', 'top center', 'bottom right', 'top right',
-        //    'top left', 'bottom right', 'bottom left', 'top right', 'top right'
-        //],
+        textposition: 'top',
     }];
 
     const layout = {
@@ -81,4 +60,10 @@
     };
 
     Plotly.newPlot('diagram', data, layout);
+
+    const myPlot = document.getElementById('diagram');
+    myPlot.on('plotly_click', function (data) {
+        const cityName = data.points[0].text;
+        alert(`Temperature diagram in ${cityName} WIP`)
+    });
 })();
