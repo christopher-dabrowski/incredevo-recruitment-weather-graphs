e4c1336612bd052fd087ea6637f1174c93b32bc2
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-06 16:21:29 +0200

    Model table entity

--- WeatherApplication/Shared/Models/WeatherInfo.cs
+++ WeatherApplication/Shared/Models/WeatherInfo.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Azure;
+using Azure.Data.Tables;
 
 namespace Shared.Models
 {
-    internal class WeatherInfo
+    public class WeatherInfo : ITableEntity
     {
+        public string PartitionKey { get; set; } = Guid.NewGuid().ToString();
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public DateTimeOffset ForecastTime { get; set; } = DateTimeOffset.Now;
+        public double TemperatureInCelsius { get; set; }
+        public double WindSpeed { get; set; }
     }
 }
