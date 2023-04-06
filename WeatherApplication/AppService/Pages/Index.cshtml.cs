4cd2b553681e9175ced29da576309503e04e5a8f
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-06 20:30:01 +0200

    Clenup UI template

--- WeatherApplication/AppService/Pages/Index.cshtml.cs
+++ WeatherApplication/AppService/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AppService.Pages
 {
@@ -14,7 +13,6 @@ namespace AppService.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
