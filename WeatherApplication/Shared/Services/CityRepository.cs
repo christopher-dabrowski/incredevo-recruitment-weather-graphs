7d538133085b1acd5f993c4fcffda1a1ad40f715
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-06 17:09:46 +0200

    Create function for filling the cities table

--- WeatherApplication/Shared/Services/CityRepository.cs
+++ WeatherApplication/Shared/Services/CityRepository.cs
@@ -7,12 +7,12 @@ namespace Shared.Services;
 public class CityRepository
 {
     private readonly TableServiceClient _tableServiceClient;
-    private readonly TableClient _tableClient;
+    private readonly TableClient _citiesTableClient;
 
     public CityRepository(IConfiguration configuration)
     {
         _tableServiceClient = new TableServiceClient(configuration[Config.WeatherDataStorageConnectionConfigName]);
-        _tableClient = new TableClient(configuration[Config.WeatherDataStorageConnectionConfigName], Config.CityTableName);
+        _citiesTableClient = new TableClient(configuration[Config.WeatherDataStorageConnectionConfigName], Config.CityTableName);
     }
 
     public async Task EnsureCityTableExists(CancellationToken cancellationToken = default)
@@ -28,16 +28,18 @@ public class CityRepository
     {
         var initialCities = new[]
         {
-            new CityInfo { Country = "Germany", Name = "Berlin", lat = "52.5170365", lon = "13.3888599"},
-            new CityInfo { Country = "Germany", Name = "Frankfurt", lat = "50.1106444", lon = "8.6820917"},
-            new CityInfo { Country = "Denmark", Name = "Copenhagen", lat = "55.6867243", lon = "12.5700724"},
-            new CityInfo { Country = "United Kingdom", Name = "London", lat = "51.5073219", lon = "-0.1276474"},
-            new CityInfo { Country = "United Kingdom", Name = "Manchester", lat = "53.4794892", lon = "-2.2451148"},
-            new CityInfo { Country = "Belarus", Name = "Minsk", lat = "53.9024716", lon = "27.5618225"},
-            new CityInfo { Country = "Poland", Name = "Warsaw", lat = "52.2319581", lon = "21.0067249"},
-            new CityInfo { Country = "Poland", Name = "Poznań", lat = "52.4006632", lon = "16.91973259178088"},
-            new CityInfo { Country = "Poland", Name = "Bydgoszcz", lat = "53.1219648", lon = "18.0002529"},
-            new CityInfo { Country = "Poland", Name = "Gdańsk", lat = "54.42880315", lon = "18.798325902846855"},
+            new CityInfo { PartitionKey = "Germany", Country = "Germany", Name = "Berlin", lat = "52.5170365", lon = "13.3888599"},
+            new CityInfo { PartitionKey = "Germany", Country = "Germany", Name = "Frankfurt", lat = "50.1106444", lon = "8.6820917"},
+            new CityInfo { PartitionKey = "Denmark", Country = "Denmark", Name = "Copenhagen", lat = "55.6867243", lon = "12.5700724"},
+            new CityInfo { PartitionKey = "United Kingdom", Country = "United Kingdom", Name = "London", lat = "51.5073219", lon = "-0.1276474"},
+            new CityInfo { PartitionKey = "United Kingdom", Country = "United Kingdom", Name = "Manchester", lat = "53.4794892", lon = "-2.2451148"},
+            new CityInfo { PartitionKey = "Belarus", Country = "Belarus", Name = "Minsk", lat = "53.9024716", lon = "27.5618225"},
+            new CityInfo { PartitionKey = "Poland", Country = "Poland", Name = "Warsaw", lat = "52.2319581", lon = "21.0067249"},
+            new CityInfo { PartitionKey = "Poland", Country = "Poland", Name = "Poznań", lat = "52.4006632", lon = "16.91973259178088"},
+            new CityInfo { PartitionKey = "Poland", Country = "Poland", Name = "Bydgoszcz", lat = "53.1219648", lon = "18.0002529"},
+            new CityInfo { PartitionKey = "Poland", Country = "Poland", Name = "Gdańsk", lat = "54.42880315", lon = "18.798325902846855"},
         };
+
+        await Task.WhenAll(initialCities.Select(city => _citiesTableClient.AddEntityAsync(city, cancellationToken)).ToList());
     }
 }
